1bfa52e22e2ed82f1eededa90d850696f10d2b23
abhisha <abhishasharma004@gmail.com>
2024-12-09 16:21:45 +0545

    random

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
+// src/app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'administrative',
+    loadChildren: () => import('./features/administrative/administrative.module').then(m => m.AdministrativeModule)
+  },
+  {
+    path: '',
+    redirectTo: '/admin',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/admin'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
