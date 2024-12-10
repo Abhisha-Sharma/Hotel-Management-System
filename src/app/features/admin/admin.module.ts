6c331b646ac73b4ca4bb6e13fed726ea6935ccc6
abhisha <abhishasharma004@gmail.com>
2024-12-10 13:23:39 +0545

    initial

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { UserManagementComponent } from './user-management/user-management.component';
+import { SettingManagementComponent } from './setting-management/setting-management/setting-management.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    UserManagementComponent,
+    SettingManagementComponent
   ],
   imports: [
     CommonModule
---
+++ src/app/features/admin/setting-management/setting-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-setting-management',
+  standalone: false,
+  
+  templateUrl: './setting-management.component.html',
+  styleUrl: './setting-management.component.scss'
+})
+export class SettingManagementComponent {
+
+}
